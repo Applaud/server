642341d00866f55cc79cc3debdf291095fb47d54
Luke Lovett <fractalbeach@gmail.com>
2012-06-15 01:11:41 -0600

    Added jquery. Also, static files can be put in /applaud/static

--- applaud/applaud/urls.py
+++ applaud/applaud/urls.py
@@ -4,11 +4,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 import views
+import settings
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'applaud.views.home', name='home'),
     # url(r'^applaud/', include('applaud.foo.urls')),
+
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
