e77a62227aa5d586a2d71a4b0785c878fdadfa6b
shahabraza <raza.shahab@gmail.com>
2012-06-25 00:57:45 +0000

    added business.html template

--- applaud/applaud/business_urls.py
+++ applaud/applaud/business_urls.py
@@ -12,6 +12,22 @@ urlpatterns = patterns('',
                        url(r'^welcome/', views.business_welcome),
                        
                        # Employee stuff
+                       url(r'^business_manage_employees/',
+                           direct_to_template,
+                           {'template':'manage_employees.html'},
+                           name='business_manage_employees'),
+                       
+                       url(r'^business_manage_newsfeed/',
+                           direct_to_template,
+                           {'template':'fail.html'},
+                           name='business_manage_newsfeed'),
+
+                       url(r'^business_manage_survey/',
+                           direct_to_template,
+                           {'template':'fail.html'},
+                           name='business_manage_survey'),
+
+
                        url(r'^edit_employee/', views.edit_employee),
                        url(r'^delete_employee/', views.delete_employee),
 
@@ -38,7 +54,5 @@ urlpatterns = patterns('',
                        url(r'^delete_newsfeed_item/', views.delete_newsfeed_item),
                        
                        # Checking analytics.
-                       url(r'^analytics/', views.analytics),
+                       url(r'^analytics/', views.analytics, name="analytics"),
                        )
-
-
