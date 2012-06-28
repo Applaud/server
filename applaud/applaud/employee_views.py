198900727ac1178eaa5084622bace88c42c15120
Luke Lovett <fractalbeach@gmail.com>
2012-06-28 10:36:55 -0700

    Applied the 'employee_view' decorator on all views in employee_views.py

--- applaud/applaud/employee_views.py
+++ applaud/applaud/employee_views.py
@@ -13,8 +13,8 @@ from applaud.models import EmployeeProfile
 # 'employee_view' decorator.
 def employee_view(view):
     '''
-    Checks a BusinessView to make sure that a user is logged in and
-    is in fact a business (i.e., has a BusinessProfile) before the
+    Checks an EmployeeView to make sure that a user is logged in and
+    is in fact an employee (i.e., has an EmployeeProfile) before the
     view is executed. If either of these tests fail, the user is redirected
     to the appropriate page.
     '''
@@ -37,117 +37,93 @@ def employee_view(view):
             
     return wrapper
 
+@employee_view
 def employee_stats(request):
     '''Gives statistics for a particular employee (given in request). This
     is accessed through the apatapa website when an employee is logged in.
     '''
-    if request.user.is_authenticated():
-        profile = ""
-        employee = ""
-	try:
-            employee = request.user
-	    profile = employee.employeeprofile
-	except EmployeeProfile.DoesNotExist:
-	    return HttpResponseNotFound("Could not find the requested page.")
-        rating_profile = profile.rating_profile
-        if not rating_profile:
-            # Employer hasn't created a ratingprofile
-            return HttpResponseRedirect("/")
-        ratings = profile.rating_set
+    employee = request.user
+    profile = employee.employeeprofile
 
-        # List of valid dimensions for rating
-        dimensions = rating_profile.dimensions
+    ratings = profile.rating_set
 
-        success_chart = []
-        axis = ['dimension', 'poor', 'fair', 'good', 'excellent', 'glorious']
-        success_chart.append(axis)
-        for i in range(len(dimensions)):
-            row = [ dimensions[i] ]
-            rating_vals = []
-            for j in range(5):
-                rating_vals.append(len(ratings.filter(title=dimensions[i],
-                                                      rating_value=j+1)))
-            row.extend(rating_vals)
-            success_chart.append(row)
+    # List of valid dimensions for rating
+    dimensions = rating_profile.dimensions
 
-        imagepath = "%s.%d"%(profile.business.user.username.replace(" ","_"),
-                             profile.business.id)
+    success_chart = []
+    axis = ['dimension', 'poor', 'fair', 'good', 'excellent', 'glorious']
+    success_chart.append(axis)
+    for i in range(len(dimensions)):
+        row = [ dimensions[i] ]
+        rating_vals = []
+        for j in range(5):
+            rating_vals.append(len(ratings.filter(title=dimensions[i],
+                                                  rating_value=j+1)))
+        row.extend(rating_vals)
+        success_chart.append(row)
 
-        imagename = "%s_%s.%d.%s"%(employee.first_name,
-                                   employee.last_name,
-                                   profile.id,
-                                   profile.profile_picture.name.split('.')[-1])
+    imagepath = "%s.%d"%(profile.business.user.username.replace(" ","_"),
+                         profile.business.id)
 
-        sys.stderr.write(imagepath)
-        sys.stderr.write(imagename)
+    imagename = "%s_%s.%d.%s"%(employee.first_name,
+                               employee.last_name,
+                               profile.id,
+                               profile.profile_picture.name.split('.')[-1])
 
-        # Return string for rendering in google charts
-        return render_to_response('employee_stats.html',
-                                  {'chartdata':json.dumps( success_chart ),
-                                   'employee':employee,
-                                   'image':"%s/%s"%(imagepath,imagename)},
-                                  context_instance=RequestContext(request))
+    # Return string for rendering in google charts
+    return render_to_response('employee_stats.html',
+                              {'chartdata':json.dumps( success_chart ),
+                               'employee':employee,
+                               'image':"%s/%s"%(imagepath,imagename)},
+                              context_instance=RequestContext(request))
 
-    return HttpResponseForbidden("employee not logged in")
-
+@employee_view
 @csrf_protect
 def edit_profile(request):
-    if request.user.is_authenticated():
-        try:
-            profile = request.user.employeeprofile
-        except:
-            return HttpResponseRedirect('/')
-        if request.method == 'POST':
-            form = registration_forms.EmployeeProfileForm(request.POST,
-                                                          request.FILES,
-                                                          instance=profile)
-            if form.is_valid():
-                form.save()
+    profile = request.user.employeeprofile
+    if request.method == 'POST':
+        form = registration_forms.EmployeeProfileForm(request.POST,
+                                                      request.FILES,
+                                                      instance=profile)
+        if form.is_valid():
+            form.save()
 
-                # ---------- HANDLE IMAGE UPLOAD ----------
-                # Set the image name based upon employeeprofile id and business id
-                # imagename = employeefn_employeeln.employeeid.fileext
-                # imagedir = MEDIA_ROOT/businessname.businessid
-                fileext = request.FILES['profile_picture'].name.split('.')[-1]
-                imagedir = "%s%s.%d"%(settings.MEDIA_ROOT,
-                                      profile.business.user.username.replace(" ","_"),
-                                      profile.business.id)
-                if not os.path.exists(imagedir):
-                    os.makedirs(imagedir)
-                imagename = "%s_%s.%d.%s"%(profile.user.first_name,
-                                           profile.user.last_name,
-                                           profile.id,
-                                           fileext)
-                imagepath = "%s/%s"%(imagedir,imagename) 
-                with open( imagepath, "wb+" ) as destination:
-                    for chunk in request.FILES['profile_picture']:
-                        destination.write(chunk)
-                destination.close()
+            # ---------- HANDLE IMAGE UPLOAD ----------
+            # Set the image name based upon employeeprofile id and business id
+            # imagename = employeefn_employeeln.employeeid.fileext
+            # imagedir = MEDIA_ROOT/businessname.businessid
+            fileext = request.FILES['profile_picture'].name.split('.')[-1]
+            imagedir = "%s%s.%d"%(settings.MEDIA_ROOT,
+                                  profile.business.user.username.replace(" ","_"),
+                                  profile.business.id)
+            if not os.path.exists(imagedir):
+                os.makedirs(imagedir)
+            imagename = "%s_%s.%d.%s"%(profile.user.first_name,
+                                       profile.user.last_name,
+                                       profile.id,
+                                       fileext)
+            imagepath = "%s/%s"%(imagedir,imagename) 
+            with open( imagepath, "wb+" ) as destination:
+                for chunk in request.FILES['profile_picture']:
+                    destination.write(chunk)
+            destination.close()
 
-                # Generate a thumbnail
-                thumb = PImage.open( imagepath )
-                thumb.thumbnail((128,128), PImage.ANTIALIAS)
-                thumb.save( "%s/thumb_%s"%(imagedir,imagename) )
-                
-                return HttpResponseRedirect('/employee/profilesuccess/')
-        else:
-            form = registration_forms.EmployeeProfileForm(instance=profile)
-        return render_to_response('employee_profile.html',
-                                  {'form':form},
-                                  context_instance=RequestContext(request))
+            # Generate a thumbnail
+            thumb = PImage.open( imagepath )
+            thumb.thumbnail((128,128), PImage.ANTIALIAS)
+            thumb.save( "%s/thumb_%s"%(imagedir,imagename) )
 
+            return HttpResponseRedirect('/employee/profilesuccess/')
+    else:
+        form = registration_forms.EmployeeProfileForm(instance=profile)
+    return render_to_response('employee_profile.html',
+                              {'form':form},
+                              context_instance=RequestContext(request))
+
+@employee_view
 def welcome(request):
     '''Welcomes an employee the first time that they log in
     '''
-    if request.user.is_authenticated():
-        profile = ""
-        employee = ""
-	try:
-            employee = request.user
-	    profile = employee.employeeprofile
-	except EmployeeProfile.DoesNotExist:
-	    return HttpResponseNotFound("Could not find the requested page.")
-        
-        return render_to_response("employee_welcome.html",
-                                  {"employee":profile})
-                               
+    return render_to_response("employee_welcome.html",
+                              {"employee":profile})
+
