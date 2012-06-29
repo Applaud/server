f17edd87b34f50b7fc72bd058065952b5a2a25b6
rkearney <rkearney@rkearney-VirtualBox.(none)>
2012-06-29 05:18:29 -0700

    Maybe minor changes

--- applaud/applaud/static/manage_employees.js
+++ applaud/applaud/static/manage_employees.js
@@ -43,7 +43,7 @@ if (! apatapa.employee) {
      */
     _ns.listEmployees = function(data, container) {
 	// Clear the current list
-	$('#employees_listing').empty();
+	container.empty();
 	var listing = $('<ul></ul>');
 
 	for ( e in data.employee_list ) {
@@ -63,7 +63,7 @@ if (! apatapa.employee) {
 	    listing.append(listitem);
 	}
 	
-	$('#employees_listing').append(listing);
+	container.append(listing);
 	bind_delete_buttons();
     };
 
