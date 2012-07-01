93aecdb994b616bd4e6dd825c4422da7bec47445
peter-fogg <peter.fogg@oberlin.edu>
2012-07-01 16:04:35 -0700

    finished jquery-ui-ifying manage_survey.js, as well as some bug fixes and cleanup

--- applaud/applaud/static/manage_newsfeed.js
+++ applaud/applaud/static/manage_newsfeed.js
@@ -65,23 +65,6 @@ if ( !apatapa.newsfeed) {
     };
     
     /*
-     * Shows an alert with an OK and cancel button, kinda like on iOS. The OK button
-     * runs the ok_func function.
-     */
-    apatapa.showAlert = function (title, message, ok_func) {
-	var alert = $('<div></div>');
-	alert.prop({'title': title});
-	alert.html('<p>' + message + '<p>');
-	alert.dialog({buttons: [
-	    {text: "OK",
-	     click: function () { ok_func();
-				  alert.dialog('close'); }},
-	    {text: "Cancel",
-	     click: function () { alert.dialog('close'); }},
-	]});
-    };
-    
-    /*
      * Save the newsfeed -- just collects all the information from the DOM and
      * sends it off through AJAX.
      */
