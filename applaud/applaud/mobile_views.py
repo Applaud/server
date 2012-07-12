1722fb3fedfefa076da9f227eadc05bd39656f88
peter-fogg <peter.fogg@oberlin.edu>
2012-07-11 22:10:04 -0700

    fixing up for keith

--- applaud/applaud/mobile_views.py
+++ applaud/applaud/mobile_views.py
@@ -157,6 +157,7 @@ def get_survey(request):
     '''
     business_id = json.load(request)['business_id']
     business = models.BusinessProfile(id=business_id)
+    print business_id
     survey = business.survey_set.all()[0]
     print survey
     questions = []
@@ -206,24 +207,9 @@ def example2(request):
     return HttpResponse(json.dumps(res))
 
 def example3(request):
-    bf = models.BusinessProfile.objects.get(id=1)
+    keith = models.BusinessProfile.objects.get(id=3)
     res = { "nearby_businesses":
-        	[
-            {"name":"Foo Burgers",
-             "type":"Burgers",
-             "goog_id":"677679492a58049a7eae079e0890897eb953d79b",
-             "latitude":39.981634,
-             "longitude":-83.004617,
-             'primary': bf.primary_color,
-             'secondary': bf.secondary_color,
-             "id": 1},
-            {"name":"Seymour House of Smiles",
-             "type":"Orthodontist",
-             "goog_id":"27ea39c8fed1c0437069066b8dccf958a2d06f19",
-             "latitude":39.981934,
-             "longitude":-83.004676,
-             "id": 1}, # ids are the same for testing purposes...
-            ],
+        	[BusinessProfileEncoder().default(keith)],
             }
     return HttpResponse(json.dumps(res))
 
