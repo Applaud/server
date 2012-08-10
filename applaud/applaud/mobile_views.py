f9191cdea057d236f6d10e094b4339cd8760add4
Luke Lovett <fractalbeach@gmail.com>
2012-08-09 23:33:52 -0700

    Post submission re-sends all posts for the thread.

--- applaud/applaud/mobile_views.py
+++ applaud/applaud/mobile_views.py
@@ -15,7 +15,7 @@ from applaud import forms
 from applaud import models
 from applaud.models import UserProfile
 from registration import forms as registration_forms
-from views import BusinessProfileEncoder, EmployeeEncoder, SurveyEncoder, QuestionEncoder, NewsFeedItemEncoder, BusinessPhotoEncoder, SimplePollEncoder, ThreadEncoder
+from views import BusinessProfileEncoder, EmployeeEncoder, SurveyEncoder, QuestionEncoder, NewsFeedItemEncoder, BusinessPhotoEncoder, SimplePollEncoder, ThreadEncoder, ThreadPostEncoder
 from business_views import save_image
 from django.utils.timezone import utc
 
@@ -626,7 +626,7 @@ def submit_post(request):
                              user=request.user.userprofile,
                              thread=thread)
     post.save()
-    return HttpResponse("")
+    return HttpResponse(json.dumps(list(thread.threadpost_set.all()), cls=ThreadPostEncoder))
 
     
 # Getting and posting employee data from iOS.
