0ccae22d34b36c9752f4e5076db610f1b6940b63
Luke Lovett <fractalbeach@gmail.com>
2012-08-13 10:33:55 -0700

    Empty employee bio is rendered in JSON as empty string, not null.

--- applaud/applaud/views.py
+++ applaud/applaud/views.py
@@ -146,7 +146,7 @@ class EmployeeEncoder(json.JSONEncoder):
                 image_url = settings.MEDIA_URL+image_url
 	    res = {'first_name':o.user.first_name,
 		   'last_name':o.user.last_name,
-		   'bio':o.bio,
+		   'bio':o.bio if o.bio else "",
 		   'ratings':
 		       {'rating_title':"" if o.rating_profile.title is None else o.rating_profile.title,
 			'dimensions':dimension_list},
