5d06255aad15d6496a7799756304c4ba2ed5ece4
Jack Jones <robertjohnkearney@gmail.com>
2012-08-14 12:40:53 -0500

    Nothing....

--- applaud/populate.py
+++ applaud/populate.py
@@ -419,8 +419,6 @@ poll1.save()
 poll2 = models.Poll(title="What is your favorite tropical location?",
                    business=keith_business,
                    options=['Hawaii', 'Florida', 'Bali', 'The Bahamas'])
-thread.votes.add(vote2)
-thread.votes.add(vote3)
 poll2.save()
 
 poll3 = models.Poll(title="Do you prefer snorkeling or scuba diving?",
@@ -578,8 +576,9 @@ create_poll_response(poll6, 0)
 thread = models.Thread(title="Tell us about your favorite mahi mahi recipe.",
                        user_creator=userprofile2,
                        business=keith_business)
-thread.votes.add(vote1)
 thread.save()
+thread.votes.add(vote1)
+
 
 
 # Make a thread post
@@ -598,10 +597,10 @@ post2.save()
 thread2 = models.Thread(title="Fun drink ideas.",
                        user_creator=userprofile2,
                        business=keith_business)
-thread.votes.add(vote2)
-thread.votes.add(vote3)
-thread.votes.add(vote1)
-thread.save()
+thread2.save()
+thread2.votes.add(vote2)
+thread2.votes.add(vote3)
+thread2.votes.add(vote1)
 
 # Make a thread post
 post1 = models.ThreadPost(body="Rum, tequila, orange juice, cranberry juice, blended",
