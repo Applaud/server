feed4cd6a36b887b74c7c31f06a0d8f8adbf96b1
Jack Jones <robertjohnkearney@gmail.com>
2012-08-21 01:01:10 -0500

    Lots of changes to HTML, CSS, and JS on the front page

--- applaud/applaud/static/frontpage.js
+++ applaud/applaud/static/frontpage.js
@@ -1,41 +1,212 @@
-$(document).ready( function(){
-    bindForm();
+/*
+ * frontPage.js - The javascript file for the frontpage of apatapa.com
+ *
+ *
+ */
 
-});
+var frontPage = frontPage || {};
 
-var bindForm = function(){
-    var defaultTextDict = {'email':"Email",
+(function (frontPage) {
+    // Dictionary associating 'name' attributes of the text input fields with their default text
+    frontPage.defaultText = {'email':"Email",
                            'first':"First name",
                            'last':"Last name (optional)",
                            'password':"Password"};
-    var selector = $('input[type=text]');
-    selector.focus( function(){
-        var name = $(this).attr('name');
-        var text = defaultTextDict[name];
-        if($(this).val() === text ){
-            $(this).val("");
-        }
-    });
-    selector.blur( function(){
-        var name = $(this).attr('name');
-        var text = defaultTextDict[name];
-        if($(this).val() === ""){
-            $(this).val(text);
-        }
-    });
-}
 
-var validateForm = function(){
-    // Check for valid email
-    var email = $('input[name="email"]').val().toLowerCase();
-    console.log(email);
-    var patt = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-    if(patt.test(email)){
-        $('input[name="email"]').addClass("error");
+    // The jQuery objects of any invalid form elements
+    frontPage.errorElements = [];
+
+    frontPage.clearErrors = function(){
+        // Remove the list of errors from the div
+        $('#error-div ul').empty();
+
+        // Get rid of the other error div styling
+        $('#error-div').css("padding-top","0px");
+        $('#error-div').css("padding-bottom","0px");
+        $('#error-div').css("margin-bottom","0px");
+        
+        // Remove the list of errors from the errorElements array
+        frontPage.errorElements = [];
+
+        // Remove the error class (and borders) from each of the invalid fields
+        $('#email-control-group').removeClass('error');
+        $('#first-control-group').removeClass('error');
+        $('#password-control-group').removeClass('error');
     }
-    else{
-        $('input[name="email"]').removeClass("error");
+
+    frontPage.bindClearErrorHandlers = function(){
+        // Reset the errors when...
+
+        // Window loses focus
+        $(window).blur( function(event){
+            //frontPage.clearErrors();
+        });
+        
+        // Any of the invalid fields are edited
+        $(':input[type=text]').keydown( function(event){
+            if( $.inArray( $(this), frontPage.errorElements ) ){
+                frontPage.clearErrors();
+            }
+        });
+
+
     }
-}
 
+    frontPage.bindForm = function(){
+        $('#register-button').click( function(event){
+            event.preventDefault();
+            frontPage.clearErrors();
 
+            // Form is valid, proceed with registration
+            if(frontPage.validateForm()){
+                console.log("it's valid");
+            }
+        });
+
+        var selector = $('input[type=text]');
+
+        // Remove default text when field gains focus
+        selector.focus( function(){
+            var name = $(this).attr('name');
+            var text = frontPage.defaultText[name];
+            if($(this).val() === text ){
+                $(this).val("");
+            }
+        });
+
+        // Insert default text when field loses focus
+        selector.blur( function(){
+            var name = $(this).attr('name');
+            var text = frontPage.defaultText[name];
+            if($(this).val() === ""){
+                $(this).val(text);
+            }
+        });
+
+        // When email goes out of focus, immediately check it on the server
+        $("input[name='email']").blur(function(){
+            frontPage.checkIfEmailUsed();
+        });
+
+    }
+
+    frontPage.validateUserType = function(){
+        var userType = $('.radio-control-group :checked').val();
+
+        // Check to see if user type has been selected
+        if( ! userType ){
+            var errorElement = $('<li></li>');
+            errorElement.text("Please select 'User' or 'Business'.");
+            $('#error-div ul').append(errorElement);
+            frontPage.errorElements.push($('.radio-control-group'));       
+        }
+    }
+
+    frontPage.validateEmail = function(){
+        var email = $('input[name="email"]').val();        
+        // Check for valid email
+        var patt = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+        if( ! patt.test(email) ){
+              $('#email-control-group').addClass("error");
+            var errorElement = $('<li></li>');
+            errorElement.text("Invalid email.");
+            $('#error-div ul').append(errorElement);
+            // If not already in the error array, add it
+            if( ! $.inArray($('#email-control-group :input'), frontPage.errorElements) ){
+                frontPage.errorElements.push($('#email-control-group :input'));
+                var exMarkImage = $('<img src="media/ExMark.png" alt="Ex Mark" />');
+                $('#email-status-image').append(exMarkImage);
+            }
+            return false;
+        }
+        return true;
+    }
+    
+    // Makes a get request to the server to check if an account with a particular email address already exists
+    frontPage.checkIfEmailUsed = function(){
+        var email = $('input[name="email"]').val();
+
+        // Check to see if email is already being used
+        $.get('mobile/check_email/',
+              {email:email},
+              function(d){
+                  var data = JSON.parse(d);
+
+                  // The email already exists
+                  if( data['does_exist'] ){
+                      var errorElement = $('<li></li>');
+                      errorElement.text("An account with that email address already exists.");
+                      $('#error-div ul').append(errorElement);
+                      // If this is not already in the error array, add it
+                      if( ! $.inArray($('#email-control-group :input'), frontPage.errorElements) ){
+                          frontPage.errorElements.push($('#email-control-group :input'));
+                          var exMarkImage = $('<img src="media/ExMark.png" alt="Ex Mark" />');
+                          var joke = $('<p>JOKE JOKE JOKE</p>');
+                         // $('#email-status-image').append(exMarkImage);
+                          $('#email-status-image').append(joke);
+                      }
+
+                      return false;
+                  }
+                  return true;
+              });
+    }
+
+    frontPage.validateFirstName = function(){
+        var firstName = $('input[name="first"]').val();
+
+        // Check First Name
+        if( firstName === "" || firstName === frontPage.defaultText['first'] ){
+            $('#first-control-group').addClass("error");
+            var errorElement = $('<li></li>');
+            errorElement.text("You didn't enter a name.");
+            $('#error-div ul').append(errorElement);
+            frontPage.errorElements.push($('#first-control-group :input'));
+            return false;
+        }
+        return true;
+    }
+
+    frontPage.validatePassword = function(){
+        var password = $('input[name="password"]').val();
+
+        // Check for valid password
+        patt = /^\w{8,30}$/;
+        if( ! patt.test(password) || password === frontPage.defaultText['password']){
+            $('#password-control-group').addClass("error");
+            var errorElement = $('<li></li>');
+            
+            // If password is too short
+            if(password.length < 8){
+                errorElement.text("Password is too short (it has to be at least 8 characters).");
+            }
+
+            // If password wasn't changed
+            if( password === frontPage.defaultText['password']){
+                errorElement.text("You didn't enter a password");
+            }
+
+            // Other password checks??
+
+            $('#error-div ul').append(errorElement);
+            frontPage.errorElements.push($('#password-control-group :input'));
+            return false;
+        }
+        return true;
+    }
+
+    frontPage.validateForm = function(){
+        frontPage.validateEmail();
+        frontPage.validateFirstName();
+        frontPage.validatePassword();
+        
+        // If there are any errors, style up the error div
+        if( frontPage.errorElements.length > 0 ){
+            $("#error-div").css("padding-top", "5px");
+            $("#error-div").css("padding-bottom", "5px");
+            $("#error-div").css("margin-bottom", "5px");            
+        }        
+
+        return isValid;
+    }
+})(frontPage);
