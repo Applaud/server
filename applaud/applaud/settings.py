053fed9c849dbcc1b7baa830833998a2f5f0d45a
Jack Jones <robertjohnkearney@gmail.com>
2012-08-21 11:57:13 -0500

    Some changes to settings

--- applaud/applaud/settings.py
+++ applaud/applaud/settings.py
@@ -34,10 +34,10 @@ CWD=os.path.dirname(__file__)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': CWD+'/test.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'apatapaDB', #CWD+'/test.db',                      # Or path to database file if using sqlite3.
+        'USER': 'apatapa',                      # Not used with sqlite3.
+        'PASSWORD': 'test',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
